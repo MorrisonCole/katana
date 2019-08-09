501d849bc64003c5f7e3335a6c89942387619976
Morrison Cole <admin@morrisoncole.co.uk>
2019-08-09 14:03:07 +0900

    Initial JMDict parsing - defined Entity mappings and begun unmarshalling

---
+++ go/src/com.morrisoncole/jmdict/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"com.morrisoncole/jmdict/model"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+const jmDictPath = "jmdict.xml"
+
+func main() {
+	jmDictXml, err := os.Open(jmDictPath)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	byteValue, _ := ioutil.ReadAll(jmDictXml)
+	var jmDict model.JMDict
+
+	d := xml.NewDecoder(bytes.NewReader(byteValue))
+
+	d.Entity = model.EntityMap
+	xmlErr := d.Decode(&jmDict)
+	if xmlErr != nil {
+		fmt.Printf("error: %v", xmlErr)
+		return
+	}
+
+	out, err := json.MarshalIndent(jmDict, "", "  ")
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+	os.Stdout.Write(out)
+}
